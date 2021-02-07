9049156a089070a76404b6701a6bbd58773790b4
Alberto Miranda <codealchemist@gmail.com>
2021-02-07 01:45:27 -0300

    WIP: file types.

--- src/components/file.js
+++ src/components/file.js
@@ -1,20 +1,52 @@
 import prettyBytes from 'pretty-bytes'
 
+const fileExtMap = {
+  audio: /(mp3|ogg|wav|flac)$/i,
+  video: /(mp4|webm|avi|ogv|)$/i,
+  text: /(txt|md)$/i
+}
+const types = Object.keys(fileExtMap)
+const contentMap = {
+  audio: ({ name, url, size }) => `
+    <span class="right">
+      <audio src="${url}" controls="true"></audio>
+    </span>
+  `
+}
+
+function getFileType (filename) {
+  let fileType
+  types.some(type => {
+    const regex = fileExtMap[type]
+    if (filename.match(regex)) {
+      fileType = type
+      return true
+    }
+    return
+  })
+
+  return fileType
+}
+
+function getContent (file) {
+  const type = getFileType(file.name)
+  const content = contentMap[type] ? contentMap[type](file) : ''
+  return content
+}
+
 let altRow = false
 function file (file) {
-  // console.log('File component:', file)
+  console.log('File component:', file)
   const { name, url, size } = file
+  const content = getContent(file)
   const html = `
-    <div class="file${altRow ? ' alt' : ''}">
-      <span>${name}</span>
-      <span>${prettyBytes(size)}</span>
-      <div class="right">
-        <audio src="${url}" controls="true"></audio>
-      </div>
-    </div>
+    <span>${name}</span>
+    <span>${prettyBytes(size)}</span>
+    ${content}
   `
   altRow = !altRow
   const div = document.createElement('div')
+  div.className = `file${altRow ? ' alt' : ''}`
   div.innerHTML = html
   return div
 }
